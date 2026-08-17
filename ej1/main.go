2c02a19a97a3a24febb1b09c474181c7ca131088
Yanqui88 <nicosatragni@gmail.com>
2026-08-17 20:59:34 -0300

    ej1

---
+++ ej1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	htmlContent := `<!DOCTYPE html>
+					<html>
+					<head><title>Hola Mundo</title></head>
+					<body><h1>¡Servidor Funcionando!</h1></body>
+					</html>`
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprint(w, htmlContent)
+	})
+	port := ":8080"
+	fmt.Printf("Servidor Escuchando en %s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
+}
